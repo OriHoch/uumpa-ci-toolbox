5d5e01548925e1fc4440490b53b1e720ce187426
Ori Hoch <ori@uumpa.com>
2021-08-30 10:57:28 +0300

    allow to set repo in github_actions_install script

--- bin/github_actions_install.sh
+++ bin/github_actions_install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 COMMIT_SHA="${1}"
+REPO="${2:-OriHoch/uumpa-ci-toolbox}"
 
 pip install --upgrade pip &&\
-pip install -r "https://raw.githubusercontent.com/OriHoch/uumpa-ci-toolbox/${COMMIT_SHA}/requirements.txt" &&\
-pip install "https://github.com/OriHoch/uumpa-ci-toolbox/archive/${COMMIT_SHA}.zip#egg=uumpa-ci-toolbox"
+pip install -r "https://raw.githubusercontent.com/${REPO}/${COMMIT_SHA}/requirements.txt" &&\
+pip install "https://github.com/${REPO}/archive/${COMMIT_SHA}.zip#egg=uumpa-ci-toolbox"
