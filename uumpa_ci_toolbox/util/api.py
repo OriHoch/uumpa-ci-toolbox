807a483b65f3ec28787300499d961338a9d6bc4a
Ori Hoch <ori@uumpa.com>
2021-08-30 12:07:25 +0300

    fix util wait-for to run via bash

--- uumpa_ci_toolbox/util/api.py
+++ uumpa_ci_toolbox/util/api.py
@@ -6,7 +6,7 @@ import subprocess
 def wait_for(timeout_seconds, condition_script):
     start_time = datetime.datetime.now()
     while True:
-        if subprocess.call(condition_script, shell=True) == 0:
+        if subprocess.call(['bash', '-c', condition_script]) == 0:
             return True
         elif (datetime.datetime.now() - start_time).total_seconds() > timeout_seconds:
             return False
