38828d7f1ff3374616c3afc7ffe662ffa6d90b17
Ori Hoch <ori@uumpa.com>
2025-04-21 19:18:50 +0300

    Update github_actions_install.sh

--- bin/github_actions_install.sh
+++ bin/github_actions_install.sh
@@ -3,6 +3,8 @@
 COMMIT_SHA="${1}"
 REPO="${2:-OriHoch/uumpa-ci-toolbox}"
 
-pip install --upgrade pip &&\
+export PIP_USE_PEP517=0
+
+pip install --upgrade pip setuptools wheel &&\
 pip install -r "https://raw.githubusercontent.com/${REPO}/${COMMIT_SHA}/requirements.txt" &&\
-pip install --no-binary :all: "https://github.com/${REPO}/archive/${COMMIT_SHA}.zip#egg=uumpa-ci-toolbox"
+pip install "https://github.com/${REPO}/archive/${COMMIT_SHA}.zip#egg=uumpa-ci-toolbox"
